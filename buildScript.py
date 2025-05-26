c2f962afce8b3552e2541d0c486308cc0a20adb1
Ben Haubrich <39037440+benhaub@users.noreply.github.com>
2025-05-26 14:12:15 -0400

    Update buildScript.py

--- buildScript.py
+++ buildScript.py
@@ -67,36 +67,42 @@ def setupForPlatform(systemName):
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser(prog='buildScript.py',
-                                       description='Run cmake projects on a desktop',
+                                       description='Run cmake projects for various platforms',
                                        epilog='Created by Ben Haubrich April 19th, 2024')
   #This first positional argument holds one or more arguments (nargs='+') so that when new positional commands are add below
   #They are contained within the list of arguments for the first positional argument. In this way, a list of possible
   #commands can be searched through by the name of the commands given.
   parser.add_argument('command', type=ascii, nargs='+', default='None',
-                          help=argparse.SUPPRESS
-                     )
+                    help=argparse.SUPPRESS
+                    )
 
   parser.add_argument('clean', type=ascii, nargs='?', default='None',
-                          help='Clean up the files from the last build'
-                     )
+                    help='Clean up the files from the last build'
+                    )
   parser.add_argument('build', type=ascii, nargs='?', default='None',
-                          help='Build the project in the selected directory'
-                     )
+                    help='Build the project in the selected directory'
+                    )
   parser.add_argument('run', type=ascii, nargs='?', default='None',
-                          help='Run the executable on the current platform. Does not run for the target.'
-                     )
+                    help='Run the executable on the current platform. Does not run for the target.'
+                    )
   parser.add_argument('doxygen', type=ascii, nargs='?', default='None',
-                          help='Build the Doxygen documentation'
-                     )
+                    help='Build the Doxygen documentation'
+                    )
   parser.add_argument('valgrind', type=ascii, nargs='?', default='None',
-                          help='Run valgrind with the selected analyzer. default is memcheck.'
-                     )
+                    help='Run valgrind with the selected analyzer. default is memcheck.'
+                    )
+  parser.add_argument('clang-tidy', type=ascii, nargs='?', default='None',
+                    help='Run clang-tidy.'
+                    )
+  parser.add_argument('cppcheck', type=ascii, nargs='?', default='None',
+                    help='Run cppcheck.'
+                    )
 
-  parser.add_argument('-c', '--project-dir', default='.',
+  parser.add_argument('-d', '--project-dir', default='.',
                     help='The directory to build the project which contains a top-level CMakeLists.txt. Defaults to current directory'
                     )
   parser.add_argument('-b', '--build-type', nargs='+', type=ascii, default='Debug',
-                 help='Build version to build. Defaults to "Debug". Choose from: "Debug", "Release", "Sanitize"',
+                    help='Build version to build. Defaults to "Debug". Choose from: "Debug", "Release", "Sanitize"',
                     )
   parser.add_argument('-x', '--toolchain', nargs='?', type=ascii, default=None,
                     help='Use the specified toolchain file instead the system default.',
@@ -105,8 +111,14 @@ if __name__ == '__main__':
                     help='Compile for the target given by target'
                     )
   parser.add_argument('-v', '--valgrind-check', nargs='+', type=ascii, default='memcheck',
-                     help='Run valgrind. default is memcheck. Choose from: "memcheck", "cachegrind", "callgrind", "helgrind", "drd", "massif", "dhat"',
-                     )
+                    help='Run valgrind. default is memcheck. Choose from: "memcheck", "cachegrind", "callgrind", "helgrind", "drd", "massif", "dhat"',
+                    )
+  parser.add_argument('-c', '--clang-tidy-check', nargs='+', type=ascii, default='cppcoreguidelines-*',
+                    help='Run clang-tidy with one of the supported checks. default is cppcoreguidelines-*'
+                    )
+  parser.add_argument('-f', '--path-to-analyze', nargs='+', type=ascii, default=None,
+                    help='Path to analyze. default is None. Path may lead to a file or a directory.'
+                    )
 
   args = parser.parse_args()
 
@@ -121,33 +133,32 @@ if __name__ == '__main__':
   cmakeBuildDirectory = Path(args.project_dir + '/' + buildDirectoryName)
 
   if '\'clean\'' in args.command:
-    if cmakeBuildDirectory.exists():
-      rmtree(args.project_dir + '/' + buildDirectoryName)
+      if cmakeBuildDirectory.exists():
+          rmtree(args.project_dir + '/' + buildDirectoryName)
 
   if '\'build\'' in args.command and '\'test\'' not in args.command:
+      cmakeBuildDirectory.mkdir(parents=True, exist_ok=True)
+      chdir(buildDirectoryName)
 
-    cmakeBuildDirectory.mkdir(parents=True, exist_ok=True)
-    chdir(buildDirectoryName)
-
-    cmakeCommand = ['cmake',
-                    '-G Ninja',
-                    '-S' + '../' + args.project_dir.strip('\'')]
+      cmakeCommand = ['cmake',
+                      '-G Ninja',
+                      '-S' + '../' + args.project_dir.strip('\'')]
     
     if (args.toolchain != None):
-       cmakeCommand.append('-DCMAKE_TOOLCHAIN_FILE=' + args.toolchain.strip('\''))
+        cmakeCommand.append('-DCMAKE_TOOLCHAIN_FILE=' + args.toolchain.strip('\''))
     else:
-       cmakeCommand.extend(['-DCMAKE_C_COMPILER=' + cCompiler,
-                            '-DCMAKE_CXX_COMPILER=' + cxxCompiler])
+        cmakeCommand.extend(['-DCMAKE_C_COMPILER=' + cCompiler,
+                             '-DCMAKE_CXX_COMPILER=' + cxxCompiler])
 
     if (args.target != None):
-       cmakeCommand.append('-D' + args.target.strip('\'') + '=1')
-       if (args.target.strip('\'') == 'Tm4c123'):
-           installProgram(systemName, 'lm4flash')
+        cmakeCommand.append('-D' + args.target.strip('\'') + '=1')
+            if (args.target.strip('\'') == 'Tm4c123'):
+                installProgram(systemName, 'lm4flash')
 
     if (args.build_type[0].strip('\'').lower() == 'release'):
-      cmakeCommand.append('-DRELEASE_BUILD=1')
+        cmakeCommand.append('-DRELEASE_BUILD=1')
     elif (args.build_type[0].strip('\'').lower() == 'sanitize'):
-       cmakeCommand.append('-DSANITIZE_BUILD=1')
+        cmakeCommand.append('-DSANITIZE_BUILD=1')
 
     subprocess.run(cmakeCommand)
     subprocess.run(['ninja']) 
@@ -174,13 +185,13 @@ if __name__ == '__main__':
      #In the Doxygen configuration file, set WARN_AS_ERROR = FAIL_ON_WARNINGS
       result = subprocess.run(['doxygen', 'Doxygen/Doxyfile'])
       if result.returncode != 0:
-        print("Doxygen exited with failure. Please check the output for errors.")
-        exit(result.returncode)
+          print("Doxygen exited with failure. Please check the output for errors.")
+          exit(result.returncode)
    
   if '\'valgrind\'' in args.command:
       if (rootPermissionRequired and getuser() != 'root'):
-         print("Re-run with sudo to do valgrind tests")
-         exit()
+          print("Re-run with sudo to do valgrind tests")
+          exit()
 
       if which('valgrind') == None:
          print("Valgrind is not installed. Install it (Y/n)?")
@@ -192,4 +203,31 @@ if __name__ == '__main__':
 
       subprocess.run(['valgrind', '--tool=' + args.valgrind_check[0].strip('\''), '--track-origins=yes', '--leak-check=full', '--read-inline-info=yes', '-s', buildDirectoryName + '/' + executableName + executableSuffix])
 
+  if '\'clang-tidy\'' in args.command:
+      result = subprocess.run(['brew', '--prefix', 'llvm'], capture_output=True)
+          if 0 != result.returncode:
+              print("Clang-tidy is not installed. Install it (Y/n)?")
+              response = input()
+              if 'Y' == response:
+                  installProgram(systemName, 'llvm')
+              else:
+                  exit()
+
+          subprocess.run([result.stdout.decode('utf-8').strip() + '/bin/clang-tidy', 
+                        '-p', buildDirectoryName,
+                        '-checks=' + args.clang_tidy_check.strip('\''),
+                        '-header-filter=.*',
+                        '<add-in-files-to-anaylze-here>'])
+        
+    if '\'cppcheck\'' in args.command:
+        if which('cppcheck') == None:
+            print("Cppcheck is not installed. Install it (Y/n)?")
+            response = input()
+            if 'Y' == response:
+                installProgram(systemName, 'cppcheck')
+            else:
+                exit()
+
+        subprocess.run(['cppcheck', '--check-level=exhaustive', args.path_to_analyze[0].strip('\'')])
+       
 exit(0)
