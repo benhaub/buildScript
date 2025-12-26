2db6c4ad059864cd2923d70a85646d061bfa4131
Ben Haubrich <39037440+benhaub@users.noreply.github.com>
2025-12-26 13:28:31 -0500

    Update target check to include 'Tm4c' prefix so that it's compatible with all tm4c devices.

--- buildScript.py
+++ buildScript.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
 
         if (args.target != None):
             cmakeCommand.append('-D' + args.target.strip('\'') + '=1')
-            if (args.target.strip('\'') == 'Tm4c123'):
+            if ('Tm4c' in args.target.strip('\'')):
                 installProgram(systemName, 'lm4flash')
 
         if (args.build_type[0].strip('\'').lower() == 'release'):
